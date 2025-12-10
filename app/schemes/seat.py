d4cbf6cadbc83f116c535b2917666aea537c4369
karthik96siddhu <karthik96siddhu@gmail.com>
2025-12-10 23:37:32 +0530

    update seat schema

--- app/schemes/seat.py
+++ app/schemes/seat.py
@@ -2,13 +2,13 @@ from pydantic import BaseModel
 
 class SeatCreate(BaseModel):
     screen_id: int
-    row: str
+    rows: int
     seats_per_row: int
     
 class SeatOut(BaseModel):
     id: int
     screen_id: int
-    row: str
+    rows: int
     seat_number: int
     seat_code: str
     
