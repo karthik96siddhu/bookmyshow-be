65038118e1ae85a1c0d8f46df8c8efec2c63a494
karthik96siddhu <karthik96siddhu@gmail.com>
2025-12-23 22:34:42 +0530

    create order api

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.routes import admin_theatre, auth, screen, movie, admin_show, user_movies, admin_seat, seat_map
+from app.routes import admin_theatre, auth, screen, movie, admin_show, user_movies, admin_seat, seat_map, order
 
 app = FastAPI()
 
@@ -12,6 +12,7 @@ app.include_router(admin_show.router)
 app.include_router(user_movies.router)
 app.include_router(admin_seat.router)
 app.include_router(seat_map.router)
+app.include_router(order.router)
 
 @app.get("/")
 def root():
---
+++ app/routes/order.py
@@ -0,0 +1,67 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from app.core.auth import get_db
+from app.models.order import Order
+from app.models.booking import Booking
+from app.models.show import Show
+from app.models.seat_lock import SeatLock
+from datetime import datetime
+
+router = APIRouter(
+    prefix="/orders",
+    tags=["User - Orders"]
+)
+
+@router.post("/{show_id}/orders")
+def create_order(show_id: int, user_id: int, db: Session = Depends(get_db)):
+    
+    order = Order(user_id=user_id, show_id=show_id, status="PENDING")
+    db.add(order)
+    db.commit()
+    db.refresh(order)
+    return {
+        "order_id": order.id,
+        "status": order.status
+    }
+    
+@router.post("/orders/{order_id}/confirm")
+def confirm_order(order_id: int, payment_reference: str, db: Session = Depends(get_db)):
+    
+    order = db.query(Order).filter(Order.id == order_id).first()
+    
+    if not order:
+        return HTTPException(status_code=404, detail="Order not found")
+    
+    if order.status != "PENDING":
+        return HTTPException(status_code=400, detail="Order already processed")
+    
+    now = datetime.utcnow()
+    
+    locks = db.query(SeatLock).filter(
+        SeatLock.show_id == order.show_id,
+        SeatLock.user_id == order.user_id,
+        SeatLock.locked_until > now
+    ).all()
+    
+    if not locks:
+        raise HTTPException(status_code=400, detail="Seats locks expired")
+    
+    for lock in locks:
+        booking = Booking(
+            order_id=order.id,
+            show_id=order.show_id,
+            seat_id=lock.seat_id
+        )
+        db.add(booking)
+        db.delete(lock)
+    
+    order.status = "PAID"
+    order.payment_reference = payment_reference
+    order.payment_provider = "RAZORPAY"  # Example provider
+    
+    db.commit()
+    
+    return {
+        "message": "Booking confirmed",
+        "order_id": order.id
+    }
